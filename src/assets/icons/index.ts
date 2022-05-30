634ec3e1f8268981a629a4e40554a695b5b09c36
jynam <njy1007@gmail.com>
2022-05-30 13:08:18 +0900

    fix: 중복 제거

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,6 @@
 export { ReactComponent as IcSearch } from './icon_search.svg';
 export { default as icPlus } from './icon_plus.svg';
 export { ReactComponent as IcPlus } from './icon_plus.svg';
-export { default as icPlus } from './icon_plus.svg';
 export { ReactComponent as IcSearchGray1 } from './icon_search_gray_1.svg';
 export { ReactComponent as IcPlusGray1 } from './icon_plus_gray_1.svg';
 export { default as icSetting } from './icon_setting.svg';
