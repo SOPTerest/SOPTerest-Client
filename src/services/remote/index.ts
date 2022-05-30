b1700a00d0ad93a431d694cceef7b909bfa030be
jynam <njy1007@gmail.com>
2022-05-30 13:19:15 +0900

    chore: console.log 삭제

--- src/services/remote/index.ts
+++ src/services/remote/index.ts
@@ -5,7 +5,6 @@ import { API } from './base';
 export function remoteService(): Service {
   const getBoardDetail = async (boardID: string) => {
     const response = await API.get({ url: `/board/${boardID}` });
-    console.log(getRelativeTime(new Date(response.data.createdAt)));
     if (response.success)
       return {
         title: response.data.boardName,
