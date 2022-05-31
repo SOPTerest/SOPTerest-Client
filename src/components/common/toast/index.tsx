2e5be79c1c69265c355eb7e12491f1dc5fdb0213
jynam <njy1007@gmail.com>
2022-06-01 03:06:28 +0900

    refactor: fixed로 변경, 컴포넌트 파일 분리, 공통 스타일 지정

---
+++ src/components/common/toast/index.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { toastState } from '../../../stores';
+import useToast from '../../../hooks/useToast';
+import CompleteToast from './CompleteToast';
+
+export default function Toast() {
+  const toast = useRecoilValue(toastState);
+  const { hideToast } = useToast();
+
+  useEffect(() => {
+    if (toast.mode) hideToast();
+  }, [toast]);
+
+  switch (toast.mode) {
+    case 'COMPLETE':
+      return <CompleteToast />;
+    case null:
+    default:
+      return <></>;
+  }
+}
